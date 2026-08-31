3942db03c8128ccc1b9b72eb08894eb291bec008
yupthatpandadk <yupthatpanda@gmail.com>
2026-08-31 19:21:15 +0200

    Add phpMyAdmin one-time gateway route

--- panel/routes/web.php
+++ panel/routes/web.php
@@ -1,3 +1,9 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PhpMyAdminGatewayController;
+
+Route::get('/database-gateway/{token}', PhpMyAdminGatewayController::class)
+    ->where('token', '[A-Za-z0-9]{64}')
+    ->name('database.gateway');
+
 Route::view('/{any?}', 'app')->where('any', '.*');
