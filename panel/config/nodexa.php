ba5ade2a708caf3d294b17073f60883739b8ee15
yupthatpandadk <yupthatpanda@gmail.com>
2026-08-31 22:26:16 +0200

    Configure Nodexa GitHub updater

--- panel/config/nodexa.php
+++ panel/config/nodexa.php
@@ -4,4 +4,7 @@ return [
     'database_port' => (int) env('NODEXA_DATABASE_PORT', 3306),
     'phpmyadmin_url' => env('NODEXA_PHPMYADMIN_URL', '/phpmyadmin/'),
     'phpmyadmin_signon_session' => env('NODEXA_PHPMYADMIN_SIGNON_SESSION', 'NodexaSignon'),
+
+    'update_repository' => env('NODEXA_UPDATE_REPOSITORY', 'yupthatpandadk/Nodexa'),
+    'update_branch' => env('NODEXA_UPDATE_BRANCH', 'main'),
 ];
