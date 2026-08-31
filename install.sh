969f5590bb01a7929167b66d7c1ef5b271a85540
yupthatpandadk <yupthatpanda@gmail.com>
2026-08-31 22:29:31 +0200

    Bump Nodexa installer to 0.6.0

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
-VERSION="0.5.0"
+VERSION="0.6.0"
 REPO="${NODEXA_REPOSITORY:-yupthatpandadk/Nodexa}"
 BRANCH="${NODEXA_BRANCH:-main}"
 URL="${NODEXA_SOURCE_URL:-https://github.com/${REPO}/archive/refs/heads/${BRANCH}.zip}"
