6375a85f8bf47db326f04856d0d5e2ca9ca487e1
yupthatpandadk <yupthatpanda@gmail.com>
2026-08-31 19:59:50 +0200

    Add Database Hosts admin page

--- panel/routes/web.php
+++ panel/routes/web.php
@@ -6,4 +6,5 @@ Route::get('/database-gateway/{token}', PhpMyAdminGatewayController::class)
     ->where('token', '[A-Za-z0-9]{64}')
     ->name('database.gateway');
 
+Route::view('/admin/database-hosts', 'admin.database-hosts')->name('admin.database-hosts');
 Route::view('/{any?}', 'app')->where('any', '.*');
