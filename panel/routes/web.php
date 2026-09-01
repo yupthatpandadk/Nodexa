0f3addeed87f447fc0fea96bf822515d3babee9b
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-01 16:05:30 +0200

    Expose Admin Node Setup page

--- panel/routes/web.php
+++ panel/routes/web.php
@@ -6,6 +6,7 @@ Route::get('/database-gateway/{token}', PhpMyAdminGatewayController::class)
     ->where('token', '[A-Za-z0-9]{64}')
     ->name('database.gateway');
 
+Route::view('/admin/nodes/setup', 'admin.node-setup')->name('admin.nodes.setup');
 Route::view('/admin/database-hosts', 'admin.database-hosts')->name('admin.database-hosts');
 Route::view('/admin/servers/{server}/startup', 'admin.server-startup')->name('admin.server-startup');
 Route::view('/admin/errors', 'admin-errors')->name('admin.errors');
