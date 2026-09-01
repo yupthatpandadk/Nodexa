152d64317d6b154f7240b28e998970d5cb624daa
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-01 16:02:51 +0200

    Store Node setup preferences

--- panel/app/Models/Node.php
+++ panel/app/Models/Node.php
@@ -5,9 +5,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Node extends Model
 {
-    protected $fillable = ['name','fqdn','scheme','daemon_port','sftp_port','token','memory_mb','disk_mb','location'];
+    protected $fillable = ['name','fqdn','scheme','daemon_port','sftp_port','token','memory_mb','disk_mb','location','setup_options'];
     protected $hidden = ['token'];
-    protected $casts = ['daemon_port'=>'integer','sftp_port'=>'integer','memory_mb'=>'integer','disk_mb'=>'integer'];
+    protected $casts = [
+        'daemon_port'=>'integer',
+        'sftp_port'=>'integer',
+        'memory_mb'=>'integer',
+        'disk_mb'=>'integer',
+        'setup_options'=>'array',
+    ];
 
     public function servers() { return $this->hasMany(Server::class); }
+    public function databaseHosts() { return $this->hasMany(DatabaseHost::class); }
 }
