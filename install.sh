bbb1e4e8a35d9dc4a7bdc54f356d5dff2e540e25
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-03 22:01:29 +0200

    Bump installer to 0.14.15 for admin creation fix

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
-VERSION="0.14.14"
+VERSION="0.14.15"
 REPO="${NODEXA_REPOSITORY:-yupthatpandadk/Nodexa}"
 BRANCH="${NODEXA_BRANCH:-main}"
 URL="${NODEXA_SOURCE_URL:-https://github.com/${REPO}/archive/refs/heads/${BRANCH}.zip}"
