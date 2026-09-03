07f4d3b55ca117eee57f4de65b903a2bbff1caf9
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-03 23:52:06 +0200

    style(ui): widen Nodexa workspace

--- panel/resources/scripts/components/elements/ContentContainer.tsx
+++ panel/resources/scripts/components/elements/ContentContainer.tsx
@@ -3,8 +3,8 @@ import { breakpoint } from '@/theme';
 import tw from 'twin.macro';
 
 const ContentContainer = styled.div`
-    max-width: 1200px;
-    ${tw`mx-4`};
+    max-width: 1560px;
+    ${tw`mx-3 sm:mx-4`};
 
     ${breakpoint('xl')`
         ${tw`mx-auto`};
