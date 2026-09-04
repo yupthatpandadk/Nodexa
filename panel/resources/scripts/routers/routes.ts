38cd44f2a9440cd3b87aa431f27b4a398c45e794
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-04 18:48:11 +0200

    feat: add Plugins server route

--- panel/resources/scripts/routers/routes.ts
+++ panel/resources/scripts/routers/routes.ts
@@ -7,6 +7,7 @@ import BackupContainer from '@/components/server/backups/BackupContainer';
 import NetworkContainer from '@/components/server/network/NetworkContainer';
 import StartupContainer from '@/components/server/startup/StartupContainer';
 import FileManagerContainer from '@/components/server/files/FileManagerContainer';
+import MinecraftPluginManager from '@/components/server/plugins/MinecraftPluginManager';
 import SettingsContainer from '@/components/server/settings/SettingsContainer';
 import AccountOverviewContainer from '@/components/dashboard/AccountOverviewContainer';
 import AccountApiContainer from '@/components/dashboard/AccountApiContainer';
@@ -14,18 +15,11 @@ import AccountSSHContainer from '@/components/dashboard/ssh/AccountSSHContainer'
 import ActivityLogContainer from '@/components/dashboard/activity/ActivityLogContainer';
 import ServerActivityLogContainer from '@/components/server/ServerActivityLogContainer';
 
-// Each of the router files is already code split out appropriately — so
-// all of the items above will only be loaded in when that router is loaded.
-//
-// These specific lazy loaded routes are to avoid loading in heavy screens
-// for the server dashboard when they're only needed for specific instances.
 const FileEditContainer = lazy(() => import('@/components/server/files/FileEditContainer'));
 const ScheduleEditContainer = lazy(() => import('@/components/server/schedules/ScheduleEditContainer'));
 
 interface RouteDefinition {
     path: string;
-    // If undefined is passed this route is still rendered into the router itself
-    // but no navigation link is displayed in the sub-navigation menu.
     name: string | undefined;
     component: React.ComponentType;
     exact?: boolean;
@@ -33,12 +27,11 @@ interface RouteDefinition {
 
 interface ServerRouteDefinition extends RouteDefinition {
     permission: string | string[] | null;
+    minecraftOnly?: boolean;
 }
 
 interface Routes {
-    // All of the routes available under "/account"
     account: RouteDefinition[];
-    // All of the routes available under "/server/:id"
     server: ServerRouteDefinition[];
 }
 
@@ -79,6 +72,13 @@ export default {
             permission: 'file.*',
             name: 'Files',
             component: FileManagerContainer,
+        },
+        {
+            path: '/plugins',
+            permission: 'file.create',
+            name: 'Plugins',
+            component: MinecraftPluginManager,
+            minecraftOnly: true,
         },
         {
             path: '/files/:action(edit|new)',
