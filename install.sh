18ab043397e862336355148bd73ef03534f8ab1a
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-04 09:02:23 +0200

    chore: set installer version 0.14.29

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
-VERSION="0.14.28"
+VERSION="0.14.29"
 REPO="${NODEXA_REPOSITORY:-yupthatpandadk/Nodexa}"
 BRANCH="${NODEXA_BRANCH:-main}"
 URL="${NODEXA_SOURCE_URL:-https://github.com/${REPO}/archive/refs/heads/${BRANCH}.zip}"
