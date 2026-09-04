bb1247916781a8d9dacc9a2c350f17a9130aed96
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-04 09:22:50 +0200

    feat(api): expose egg game logo

--- panel/app/Transformers/Api/Client/EggTransformer.php
+++ panel/app/Transformers/Api/Client/EggTransformer.php
@@ -3,12 +3,10 @@
 namespace Pterodactyl\Transformers\Api\Client;
 
 use Pterodactyl\Models\Egg;
+use Illuminate\Support\Facades\Storage;
 
 class EggTransformer extends BaseClientTransformer
 {
-    /**
-     * Return the resource name for the JSONAPI output.
-     */
     public function getResourceName(): string
     {
         return Egg::RESOURCE_NAME;
@@ -19,6 +17,7 @@ class EggTransformer extends BaseClientTransformer
         return [
             'uuid' => $egg->uuid,
             'name' => $egg->name,
+            'icon' => $egg->icon_path ? Storage::disk('public')->url($egg->icon_path) : null,
         ];
     }
 }
