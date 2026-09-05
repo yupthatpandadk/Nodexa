e032465f499777976c6ae110c7d29e8a9a77db34
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-05 16:43:46 +0200

    Build Android app against Client API routes

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dk.nodexa.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
     }
 
     compileOptions {
