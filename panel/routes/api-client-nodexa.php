c2048316d72d105955430e5f63b0dff81d00f762
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-05 12:01:51 +0200

    Register per-server Minecraft mod API routes

--- panel/routes/api-client-nodexa.php
+++ panel/routes/api-client-nodexa.php
@@ -6,17 +6,30 @@ use Pterodactyl\Http\Middleware\Activity\ServerSubject;
 use Pterodactyl\Http\Middleware\Api\Client\Server\ResourceBelongsToServer;
 use Pterodactyl\Http\Middleware\Api\Client\Server\AuthenticateServerAccess;
 
+$serverMiddleware = [
+    ServerSubject::class,
+    AuthenticateServerAccess::class,
+    ResourceBelongsToServer::class,
+];
+
 Route::group([
     'prefix' => '/servers/{server}/plugins',
-    'middleware' => [
-        ServerSubject::class,
-        AuthenticateServerAccess::class,
-        ResourceBelongsToServer::class,
-    ],
+    'middleware' => $serverMiddleware,
 ], function () {
     Route::get('/search', [Client\Servers\MinecraftPluginController::class, 'search']);
     Route::get('/installed', [Client\Servers\MinecraftPluginController::class, 'installed']);
     Route::post('/install', [Client\Servers\MinecraftPluginController::class, 'install']);
     Route::delete('/{projectId}', [Client\Servers\MinecraftPluginController::class, 'uninstall'])
         ->where('projectId', '[A-Za-z0-9_-]+');
+});
+
+Route::group([
+    'prefix' => '/servers/{server}/mods',
+    'middleware' => $serverMiddleware,
+], function () {
+    Route::get('/search', [Client\Servers\MinecraftModController::class, 'search']);
+    Route::get('/installed', [Client\Servers\MinecraftModController::class, 'installed']);
+    Route::post('/install', [Client\Servers\MinecraftModController::class, 'install']);
+    Route::delete('/{filename}', [Client\Servers\MinecraftModController::class, 'uninstall'])
+        ->where('filename', '[A-Za-z0-9%._+()\\-]+');
 });
