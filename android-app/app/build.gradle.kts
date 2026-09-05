11550f0ef20ec6ea5bd954eefb3825334de9ac77
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-05 16:53:51 +0200

    Bump Android app to 0.2.0

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dk.nodexa.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.2.0"
     }
 
     compileOptions {
