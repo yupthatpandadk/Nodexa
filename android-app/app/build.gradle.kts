ebaae55248b0f5cec6b161be057e4ca9f17aa192
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-05 19:06:44 +0200

    Bump rebuilt Nodexa Android app to 0.4.0

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dk.nodexa.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.3.0"
+        versionCode = 5
+        versionName = "0.4.0"
     }
 
     compileOptions {
