ab641d618648ffe4ab17155fc65f3d2b700fa37d
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-06 14:11:12 +0200

    Add customer ticket routes

--- panel/routes/base.php
+++ panel/routes/base.php
@@ -9,9 +9,14 @@ Route::get('/account', [Base\IndexController::class, 'index'])
     ->withoutMiddleware(RequireTwoFactorAuthentication::class)
     ->name('account');
 
+Route::get('/tickets', [Base\TicketController::class, 'index'])->name('tickets.index');
+Route::post('/tickets', [Base\TicketController::class, 'store'])->name('tickets.store');
+Route::post('/tickets/{ticket}/reply', [Base\TicketController::class, 'reply'])->name('tickets.reply');
+Route::post('/tickets/{ticket}/close', [Base\TicketController::class, 'close'])->name('tickets.close');
+
 Route::get('/locales/locale.json', Base\LocaleController::class)
     ->withoutMiddleware(['auth', RequireTwoFactorAuthentication::class])
     ->where('namespace', '.*');
 
 Route::get('/{react}', [Base\IndexController::class, 'index'])
-    ->where('react', '^(?!(\/)?(api|auth|admin|daemon)).+');
+    ->where('react', '^(?!(\/)?(api|auth|admin|daemon|tickets)).+');
