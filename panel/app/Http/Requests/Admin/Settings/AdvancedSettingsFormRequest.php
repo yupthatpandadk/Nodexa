c183cbf293afc9f57c5a4c72fd333086ce8c66d1
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-07 17:02:28 +0200

    feat: validate configurable file upload limit

--- panel/app/Http/Requests/Admin/Settings/AdvancedSettingsFormRequest.php
+++ panel/app/Http/Requests/Admin/Settings/AdvancedSettingsFormRequest.php
@@ -6,9 +6,6 @@ use Pterodactyl\Http\Requests\Admin\AdminFormRequest;
 
 class AdvancedSettingsFormRequest extends AdminFormRequest
 {
-    /**
-     * Return all the rules to apply to this request's data.
-     */
     public function rules(): array
     {
         return [
@@ -18,19 +15,9 @@ class AdvancedSettingsFormRequest extends AdminFormRequest
             'pterodactyl:guzzle:timeout' => 'required|integer|between:1,60',
             'pterodactyl:guzzle:connect_timeout' => 'required|integer|between:1,60',
             'pterodactyl:client_features:allocations:enabled' => 'required|in:true,false',
-            'pterodactyl:client_features:allocations:range_start' => [
-                'nullable',
-                'required_if:pterodactyl:client_features:allocations:enabled,true',
-                'integer',
-                'between:1024,65535',
-            ],
-            'pterodactyl:client_features:allocations:range_end' => [
-                'nullable',
-                'required_if:pterodactyl:client_features:allocations:enabled,true',
-                'integer',
-                'between:1024,65535',
-                'gt:pterodactyl:client_features:allocations:range_start',
-            ],
+            'pterodactyl:client_features:allocations:range_start' => ['nullable','required_if:pterodactyl:client_features:allocations:enabled,true','integer','between:1024,65535'],
+            'pterodactyl:client_features:allocations:range_end' => ['nullable','required_if:pterodactyl:client_features:allocations:enabled,true','integer','between:1024,65535','gt:pterodactyl:client_features:allocations:range_start'],
+            'nodexa:upload_limit_mb' => 'required|integer|between:64,10240',
         ];
     }
 
@@ -45,6 +32,7 @@ class AdvancedSettingsFormRequest extends AdminFormRequest
             'pterodactyl:client_features:allocations:enabled' => 'Auto Create Allocations Enabled',
             'pterodactyl:client_features:allocations:range_start' => 'Starting Port',
             'pterodactyl:client_features:allocations:range_end' => 'Ending Port',
+            'nodexa:upload_limit_mb' => 'Maximum File Upload Size',
         ];
     }
 }
