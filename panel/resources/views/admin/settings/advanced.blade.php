0d4210e52c600bcf4375d368346ddc99a4467934
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-07 17:02:42 +0200

    feat: add upload size control to admin settings

--- panel/resources/views/admin/settings/advanced.blade.php
+++ panel/resources/views/admin/settings/advanced.blade.php
@@ -1,127 +1,14 @@
 @extends('layouts.admin')
 @include('partials/admin.settings.nav', ['activeTab' => 'advanced'])
-
-@section('title')
-    Advanced Settings
-@endsection
-
-@section('content-header')
-    <h1>Advanced Settings<small>Configure advanced settings for Pterodactyl.</small></h1>
-    <ol class="breadcrumb">
-        <li><a href="{{ route('admin.index') }}">Admin</a></li>
-        <li class="active">Settings</li>
-    </ol>
-@endsection
-
+@section('title')Advanced Settings@endsection
+@section('content-header')<h1>Advanced Settings<small>Configure advanced settings for Nodexa.</small></h1>@endsection
 @section('content')
-    @yield('settings::nav')
-    <div class="row">
-        <div class="col-xs-12">
-            <form action="" method="POST">
-                <div class="box">
-                    <div class="box-header with-border">
-                        <h3 class="box-title">reCAPTCHA</h3>
-                    </div>
-                    <div class="box-body">
-                        <div class="row">
-                            <div class="form-group col-md-4">
-                                <label class="control-label">Status</label>
-                                <div>
-                                    <select class="form-control" name="recaptcha:enabled">
-                                        <option value="true">Enabled</option>
-                                        <option value="false" @if(old('recaptcha:enabled', config('recaptcha.enabled')) == '0') selected @endif>Disabled</option>
-                                    </select>
-                                    <p class="text-muted small">If enabled, login forms and password reset forms will do a silent captcha check and display a visible captcha if needed.</p>
-                                </div>
-                            </div>
-                            <div class="form-group col-md-4">
-                                <label class="control-label">Site Key</label>
-                                <div>
-                                    <input type="text" required class="form-control" name="recaptcha:website_key" value="{{ old('recaptcha:website_key', config('recaptcha.website_key')) }}">
-                                </div>
-                            </div>
-                            <div class="form-group col-md-4">
-                                <label class="control-label">Secret Key</label>
-                                <div>
-                                    <input type="text" required class="form-control" name="recaptcha:secret_key" value="{{ old('recaptcha:secret_key', config('recaptcha.secret_key')) }}">
-                                    <p class="text-muted small">Used for communication between your site and Google. Be sure to keep it a secret.</p>
-                                </div>
-                            </div>
-                        </div>
-                        @if($showRecaptchaWarning)
-                            <div class="row">
-                                <div class="col-xs-12">
-                                    <div class="alert alert-warning no-margin">
-                                        You are currently using reCAPTCHA keys that were shipped with this Panel. For improved security it is recommended to <a href="https://www.google.com/recaptcha/admin">generate new invisible reCAPTCHA keys</a> that tied specifically to your website.
-                                    </div>
-                                </div>
-                            </div>
-                        @endif
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="box-header with-border">
-                        <h3 class="box-title">HTTP Connections</h3>
-                    </div>
-                    <div class="box-body">
-                        <div class="row">
-                            <div class="form-group col-md-6">
-                                <label class="control-label">Connection Timeout</label>
-                                <div>
-                                    <input type="number" required class="form-control" name="pterodactyl:guzzle:connect_timeout" value="{{ old('pterodactyl:guzzle:connect_timeout', config('pterodactyl.guzzle.connect_timeout')) }}">
-                                    <p class="text-muted small">The amount of time in seconds to wait for a connection to be opened before throwing an error.</p>
-                                </div>
-                            </div>
-                            <div class="form-group col-md-6">
-                                <label class="control-label">Request Timeout</label>
-                                <div>
-                                    <input type="number" required class="form-control" name="pterodactyl:guzzle:timeout" value="{{ old('pterodactyl:guzzle:timeout', config('pterodactyl.guzzle.timeout')) }}">
-                                    <p class="text-muted small">The amount of time in seconds to wait for a request to be completed before throwing an error.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="box-header with-border">
-                        <h3 class="box-title">Automatic Allocation Creation</h3>
-                    </div>
-                    <div class="box-body">
-                        <div class="row">
-                            <div class="form-group col-md-4">
-                                <label class="control-label">Status</label>
-                                <div>
-                                    <select class="form-control" name="pterodactyl:client_features:allocations:enabled">
-                                        <option value="false">Disabled</option>
-                                        <option value="true" @if(old('pterodactyl:client_features:allocations:enabled', config('pterodactyl.client_features.allocations.enabled'))) selected @endif>Enabled</option>
-                                    </select>
-                                    <p class="text-muted small">If enabled users will have the option to automatically create new allocations for their server via the frontend.</p>
-                                </div>
-                            </div>
-                            <div class="form-group col-md-4">
-                                <label class="control-label">Starting Port</label>
-                                <div>
-                                    <input type="number" class="form-control" name="pterodactyl:client_features:allocations:range_start" value="{{ old('pterodactyl:client_features:allocations:range_start', config('pterodactyl.client_features.allocations.range_start')) }}">
-                                    <p class="text-muted small">The starting port in the range that can be automatically allocated.</p>
-                                </div>
-                            </div>
-                            <div class="form-group col-md-4">
-                                <label class="control-label">Ending Port</label>
-                                <div>
-                                    <input type="number" class="form-control" name="pterodactyl:client_features:allocations:range_end" value="{{ old('pterodactyl:client_features:allocations:range_end', config('pterodactyl.client_features.allocations.range_end')) }}">
-                                    <p class="text-muted small">The ending port in the range that can be automatically allocated.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="box box-primary">
-                    <div class="box-footer">
-                        {{ csrf_field() }}
-                        <button type="submit" name="_method" value="PATCH" class="btn btn-sm btn-primary pull-right">Save</button>
-                    </div>
-                </div>
-            </form>
-        </div>
-    </div>
+@yield('settings::nav')
+<div class="row"><div class="col-xs-12"><form action="" method="POST">
+<div class="box box-primary"><div class="box-header with-border"><h3 class="box-title"><i class="fa fa-upload"></i> File Uploads</h3></div><div class="box-body"><div class="row"><div class="form-group col-md-6"><label class="control-label">Maximum File Upload Size</label><div class="input-group"><input type="number" min="64" max="10240" step="64" required class="form-control" name="nodexa:upload_limit_mb" value="{{ old('nodexa:upload_limit_mb', config('nodexa.upload_limit_mb', 2048)) }}"><span class="input-group-addon">MB</span></div><p class="text-muted small">Maximum size for browser file uploads. Recommended: 2048 MB (2 GB). Allowed range: 64 MB to 10240 MB (10 GB). Very large files are better transferred with SFTP.</p></div><div class="col-md-6"><div class="callout callout-info" style="margin-top:25px"><h4>Recommended: 2 GB</h4><p>This setting is used by Nodexa as the desired upload limit. The server's PHP and Nginx limits must also allow at least this amount.</p></div></div></div></div></div>
+<div class="box"><div class="box-header with-border"><h3 class="box-title">reCAPTCHA</h3></div><div class="box-body"><div class="row"><div class="form-group col-md-4"><label>Status</label><select class="form-control" name="recaptcha:enabled"><option value="true">Enabled</option><option value="false" @if(old('recaptcha:enabled',config('recaptcha.enabled'))=='0') selected @endif>Disabled</option></select></div><div class="form-group col-md-4"><label>Site Key</label><input type="text" required class="form-control" name="recaptcha:website_key" value="{{old('recaptcha:website_key',config('recaptcha.website_key'))}}"></div><div class="form-group col-md-4"><label>Secret Key</label><input type="text" required class="form-control" name="recaptcha:secret_key" value="{{old('recaptcha:secret_key',config('recaptcha.secret_key'))}}"></div></div></div></div>
+<div class="box"><div class="box-header with-border"><h3 class="box-title">HTTP Connections</h3></div><div class="box-body"><div class="row"><div class="form-group col-md-6"><label>Connection Timeout</label><input type="number" required class="form-control" name="pterodactyl:guzzle:connect_timeout" value="{{old('pterodactyl:guzzle:connect_timeout',config('pterodactyl.guzzle.connect_timeout'))}}"></div><div class="form-group col-md-6"><label>Request Timeout</label><input type="number" required class="form-control" name="pterodactyl:guzzle:timeout" value="{{old('pterodactyl:guzzle:timeout',config('pterodactyl.guzzle.timeout'))}}"></div></div></div></div>
+<div class="box"><div class="box-header with-border"><h3 class="box-title">Automatic Allocation Creation</h3></div><div class="box-body"><div class="row"><div class="form-group col-md-4"><label>Status</label><select class="form-control" name="pterodactyl:client_features:allocations:enabled"><option value="false">Disabled</option><option value="true" @if(old('pterodactyl:client_features:allocations:enabled',config('pterodactyl.client_features.allocations.enabled'))) selected @endif>Enabled</option></select></div><div class="form-group col-md-4"><label>Starting Port</label><input type="number" class="form-control" name="pterodactyl:client_features:allocations:range_start" value="{{old('pterodactyl:client_features:allocations:range_start',config('pterodactyl.client_features.allocations.range_start'))}}"></div><div class="form-group col-md-4"><label>Ending Port</label><input type="number" class="form-control" name="pterodactyl:client_features:allocations:range_end" value="{{old('pterodactyl:client_features:allocations:range_end',config('pterodactyl.client_features.allocations.range_end'))}}"></div></div></div></div>
+<div class="box box-primary"><div class="box-footer">{{csrf_field()}}<button type="submit" name="_method" value="PATCH" class="btn btn-sm btn-primary pull-right">Save Changes</button></div></div>
+</form></div></div>
 @endsection
