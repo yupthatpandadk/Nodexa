06d0c1d55387d66f4e51f43fb75837f9f7bc1413
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-07 15:33:41 +0200

    feat: route authenticated users to Nodexa client area

--- panel/routes/base.php
+++ panel/routes/base.php
@@ -2,9 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use Pterodactyl\Http\Controllers\Base;
+use Pterodactyl\Http\Controllers\ClientAreaController;
 use Pterodactyl\Http\Middleware\RequireTwoFactorAuthentication;
 
 Route::get('/', [Base\IndexController::class, 'index'])->name('index')->fallback();
+
+// WHMCS-style Nodexa customer area. It uses the same authenticated account as
+// the server panel, so customers never need a second login.
+Route::get('/client', [ClientAreaController::class, 'index'])
+    ->middleware('auth')
+    ->name('client.area');
+
 Route::get('/account', [Base\IndexController::class, 'index'])
     ->withoutMiddleware(RequireTwoFactorAuthentication::class)
     ->name('account');
@@ -14,4 +22,4 @@ Route::get('/locales/locale.json', Base\LocaleController::class)
     ->where('namespace', '.*');
 
 Route::get('/{react}', [Base\IndexController::class, 'index'])
-    ->where('react', '^(?!(\/)?(api|auth|admin|daemon)).+');
+    ->where('react', '^(?!(\/)?(api|auth|admin|daemon|client)).+');
