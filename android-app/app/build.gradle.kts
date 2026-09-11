d0712ffa62da3545115c199232b2f1a1b64ebf00
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-11 20:16:57 +0200

    Bump Nodexa Android app to 3.0

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dk.nodexa.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = System.getenv("GITHUB_RUN_NUMBER")?.toIntOrNull()?.plus(2000) ?: 2000
-        versionName = "2.0.0"
+        versionCode = System.getenv("GITHUB_RUN_NUMBER")?.toIntOrNull()?.plus(3000) ?: 3000
+        versionName = "3.0.0"
     }
 
     compileOptions {
