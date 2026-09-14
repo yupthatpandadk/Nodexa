651c43a3d8f7a25cde988a2de6bc91ca59c765ee
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-14 17:53:08 +0200

    Add Discord Bot configuration

--- panel/config/nodexa.php
+++ panel/config/nodexa.php
@@ -10,4 +10,14 @@ return [
         'redirect_uri' => env('NODEXA_DISCORD_REDIRECT_URI'),
         'after_login' => env('NODEXA_DISCORD_AFTER_LOGIN', '/client'),
     ],
+
+    'discord_bot' => [
+        'enabled' => env('NODEXA_DISCORD_BOT_ENABLED', false),
+        'token' => env('NODEXA_DISCORD_BOT_TOKEN'),
+        'client_id' => env('NODEXA_DISCORD_BOT_CLIENT_ID'),
+        'guild_id' => env('NODEXA_DISCORD_BOT_GUILD_ID'),
+        'status_channel_id' => env('NODEXA_DISCORD_BOT_STATUS_CHANNEL_ID'),
+        'auto_role_id' => env('NODEXA_DISCORD_BOT_AUTO_ROLE_ID'),
+        'presence' => env('NODEXA_DISCORD_BOT_PRESENCE', 'Nodexa Hosting'),
+    ],
 ];
