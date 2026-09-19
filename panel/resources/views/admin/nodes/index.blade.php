d739dfa56894c7f09489ec9ee52cae3e10055e67
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-19 23:42:14 +0200

    Prevent invalid encrypted node key from crashing Nodes page

--- panel/resources/views/admin/nodes/index.blade.php
+++ panel/resources/views/admin/nodes/index.blade.php
@@ -38,24 +38,25 @@
             <div class="box-body table-responsive no-padding">
                 <table class="table table-hover">
                     <tbody>
-                        <tr>
-                            <th></th>
-                            <th>Name</th>
-                            <th>Location</th>
-                            <th>Memory</th>
-                            <th>Disk</th>
-                            <th class="text-center">Servers</th>
-                            <th class="text-center">SSL</th>
-                            <th class="text-center">Public</th>
-                        </tr>
+                        <tr><th></th><th>Name</th><th>Location</th><th>Memory</th><th>Disk</th><th class="text-center">Servers</th><th class="text-center">SSL</th><th class="text-center">Public</th></tr>
                         @foreach ($nodes as $node)
+                            @php
+                                // A changed APP_KEY must never make the entire Nodes page return HTTP 500.
+                                // If a legacy daemon secret cannot be decrypted, mark this node offline.
+                                $nodePingSecret = null;
+                                try {
+                                    $nodePingSecret = $node->getDecryptedKey();
+                                } catch (\Illuminate\Contracts\Encryption\DecryptException $exception) {
+                                    \Illuminate\Support\Facades\Log::warning('Unable to decrypt daemon key for node; node will be shown offline until its credentials are regenerated.', ['node_id' => $node->id]);
+                                } catch (\Throwable $exception) {
+                                    \Illuminate\Support\Facades\Log::warning('Unable to load daemon key for node.', ['node_id' => $node->id, 'error' => $exception->getMessage()]);
+                                }
+                            @endphp
                             <tr>
-                                <td class="text-center text-muted left-icon" data-action="ping" data-secret="{{ $node->getDecryptedKey() }}" data-location="{{ $node->scheme }}://{{ $node->fqdn }}:{{ $node->daemonListen }}/api/system"><i class="fa fa-fw fa-refresh fa-spin"></i></td>
+                                <td class="text-center text-muted left-icon" @if($nodePingSecret) data-action="ping" data-secret="{{ $nodePingSecret }}" data-location="{{ $node->scheme }}://{{ $node->fqdn }}:{{ $node->daemonListen }}/api/system" @else title="Node credentials need to be regenerated." @endif><i class="fa fa-fw {{ $nodePingSecret ? 'fa-refresh fa-spin' : 'fa-heart-o' }}"></i></td>
                                 <td>{!! $node->maintenance_mode ? '<span class="label label-warning"><i class="fa fa-wrench"></i></span> ' : '' !!}<a href="{{ route('admin.nodes.view', $node->id) }}">{{ $node->name }}</a></td>
                                 <td>{{ $node->location->short }}</td>
-                                <td>{{ $node->memory }} MiB</td>
-                                <td>{{ $node->disk }} MiB</td>
-                                <td class="text-center">{{ $node->servers_count }}</td>
+                                <td>{{ $node->memory }} MiB</td><td>{{ $node->disk }} MiB</td><td class="text-center">{{ $node->servers_count }}</td>
                                 <td class="text-center" style="color:{{ ($node->scheme === 'https') ? '#50af51' : '#d9534f' }}"><i class="fa fa-{{ ($node->scheme === 'https') ? 'lock' : 'unlock' }}"></i></td>
                                 <td class="text-center"><i class="fa fa-{{ ($node->public) ? 'eye' : 'eye-slash' }}"></i></td>
                             </tr>
@@ -63,11 +64,7 @@
                     </tbody>
                 </table>
             </div>
-            @if($nodes->hasPages())
-                <div class="box-footer with-border">
-                    <div class="col-md-12 text-center">{!! $nodes->appends(['query' => Request::input('query')])->render() !!}</div>
-                </div>
-            @endif
+            @if($nodes->hasPages())<div class="box-footer with-border"><div class="col-md-12 text-center">{!! $nodes->appends(['query' => Request::input('query')])->render() !!}</div></div>@endif
         </div>
     </div>
 </div>
@@ -78,30 +75,10 @@
     <script>
     (function pingNodes() {
         $('td[data-action="ping"]').each(function(i, element) {
-            $.ajax({
-                type: 'GET',
-                url: $(element).data('location'),
-                headers: {
-                    'Authorization': 'Bearer ' + $(element).data('secret'),
-                },
-                timeout: 5000
-            }).done(function (data) {
-                $(element).find('i').tooltip({
-                    title: 'v' + data.version,
-                });
-                $(element).removeClass('text-muted').find('i').removeClass().addClass('fa fa-fw fa-heartbeat faa-pulse animated').css('color', '#50af51');
-            }).fail(function (error) {
-                var errorText = 'Error connecting to node! Check browser console for details.';
-                try {
-                    errorText = error.responseJSON.errors[0].detail || errorText;
-                } catch (ex) {}
-
-                $(element).removeClass('text-muted').find('i').removeClass().addClass('fa fa-fw fa-heart-o').css('color', '#d9534f');
-                $(element).find('i').tooltip({ title: errorText });
-            });
-        }).promise().done(function () {
-            setTimeout(pingNodes, 10000);
-        });
+            $.ajax({type:'GET',url:$(element).data('location'),headers:{'Authorization':'Bearer '+$(element).data('secret')},timeout:5000})
+            .done(function(data){$(element).find('i').tooltip({title:'v'+data.version});$(element).removeClass('text-muted').find('i').removeClass().addClass('fa fa-fw fa-heartbeat faa-pulse animated').css('color','#50af51');})
+            .fail(function(error){var errorText='Error connecting to node! Check browser console for details.';try{errorText=error.responseJSON.errors[0].detail||errorText;}catch(ex){}$(element).removeClass('text-muted').find('i').removeClass().addClass('fa fa-fw fa-heart-o').css('color','#d9534f');$(element).find('i').tooltip({title:errorText});});
+        }).promise().done(function(){setTimeout(pingNodes,10000);});
     })();
     </script>
 @endsection
