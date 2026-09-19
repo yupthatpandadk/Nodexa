a6e4584fd9088acb483cffd5091c7d8fa7139451
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-20 01:53:35 +0200

    Nodexa 1.1.0: redesign client panel

--- resources/scripts/components/elements/PageContentBlock.tsx
+++ resources/scripts/components/elements/PageContentBlock.tsx
@@ -4,42 +4,22 @@ import { CSSTransition } from 'react-transition-group';
 import tw from 'twin.macro';
 import FlashMessageRender from '@/components/FlashMessageRender';
 
-export interface PageContentBlockProps {
-    title?: string;
-    className?: string;
-    showFlashKey?: string;
-}
+export interface PageContentBlockProps { title?: string; className?: string; showFlashKey?: string; }
 
 const PageContentBlock: React.FC<PageContentBlockProps> = ({ title, showFlashKey, className, children }) => {
-    useEffect(() => {
-        if (title) {
-            document.title = title;
-        }
-    }, [title]);
-
+    useEffect(() => { document.title = title ? `${title} · Nodexa` : 'Nodexa'; }, [title]);
     return (
         <CSSTransition timeout={150} classNames={'fade'} appear in>
             <>
-                <ContentContainer css={tw`my-4 sm:my-10`} className={className}>
+                <ContentContainer css={tw`my-6 sm:my-10`} className={className}>
                     {showFlashKey && <FlashMessageRender byKey={showFlashKey} css={tw`mb-4`} />}
                     {children}
                 </ContentContainer>
-                <ContentContainer css={tw`mb-4`}>
-                    <p css={tw`text-center text-neutral-500 text-xs`}>
-                        <a
-                            rel={'noopener nofollow noreferrer'}
-                            href={'https://pterodactyl.io'}
-                            target={'_blank'}
-                            css={tw`no-underline text-neutral-500 hover:text-neutral-300`}
-                        >
-                            Pterodactyl&reg;
-                        </a>
-                        &nbsp;&copy; 2015 - {new Date().getFullYear()}
-                    </p>
+                <ContentContainer css={tw`mb-6`}>
+                    <p css={tw`text-center text-neutral-600 text-xs`}>Nodexa Control Panel &copy; {new Date().getFullYear()}</p>
                 </ContentContainer>
             </>
         </CSSTransition>
     );
 };
-
 export default PageContentBlock;
