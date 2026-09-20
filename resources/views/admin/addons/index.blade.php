a4b2bafeb51adf82678eafa3de7f5ea1eb1f6695
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-20 22:34:38 +0200

    Add Mod Manager card to Addon Manager

--- resources/views/admin/addons/index.blade.php
+++ resources/views/admin/addons/index.blade.php
@@ -28,5 +28,20 @@
             </div>
         </a>
     </div>
+    <div class="col-lg-4 col-md-5 col-sm-6 col-xs-12">
+        <a href="{{ route('admin.addons.show', $modAddon->id) }}" style="display:block;text-decoration:none;color:inherit">
+            <div class="box" style="border:1px solid #26384d;border-radius:14px;overflow:hidden;min-height:230px">
+                <div class="box-body" style="padding:24px">
+                    <div style="display:flex;justify-content:space-between;align-items:flex-start">
+                        <div style="width:52px;height:52px;border-radius:12px;background:rgba(6,182,212,.14);display:flex;align-items:center;justify-content:center;font-size:24px;color:#22d3ee"><i class="fa fa-cubes"></i></div>
+                        {!! $modAddon->enabled ? '<span class="label label-success">Aktiv</span>' : '<span class="label label-default">Deaktiveret</span>' !!}
+                    </div>
+                    <h3 style="margin:22px 0 7px;font-size:18px;color:#fff">Minecraft Mod Manager</h3>
+                    <p class="text-muted" style="line-height:1.6;margin-bottom:18px">Mods fra Modrinth med automatisk Forge, NeoForge, Fabric og Quilt filtrering.</p>
+                    <div style="display:flex;justify-content:space-between;border-top:1px solid #26384d;padding-top:15px"><span class="text-muted"><i class="fa fa-cube"></i> Minecraft</span><span style="color:#22d3ee;font-weight:600">Administrer <i class="fa fa-chevron-right"></i></span></div>
+                </div>
+            </div>
+        </a>
+    </div>
 </div>
 @endsection
