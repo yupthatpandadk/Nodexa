a3188787722c00217f21a4612ae06afdfb03f2b7
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-20 12:10:48 +0200

    Add Mail Center to admin navigation

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -87,6 +87,11 @@
                                 <i class="fa fa-gamepad"></i> <span>Application API</span>
                             </a>
                         </li>
+                        <li class="{{ ! starts_with(Route::currentRouteName(), 'admin.mail') ?: 'active' }}">
+                            <a href="{{ route('admin.mail') }}">
+                                <i class="fa fa-envelope"></i> <span>Mail Center</span>
+                            </a>
+                        </li>
                         <li class="{{ ! starts_with(Route::currentRouteName(), 'admin.updates') ?: 'active' }}">
                             <a href="{{ route('admin.updates') }}">
                                 <i class="fa fa-cloud-download"></i> <span>Update Center</span>
