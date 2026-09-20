4fac3509847ce4e18792261b32ab3690c8a312e5
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-20 18:06:56 +0200

    Allow role-based Nodexa admin access

--- app/Http/Middleware/AdminAuthenticate.php
+++ app/Http/Middleware/AdminAuthenticate.php
@@ -14,7 +14,7 @@ class AdminAuthenticate
      */
     public function handle(Request $request, \Closure $next): mixed
     {
-        if (!$request->user() || !$request->user()->root_admin) {
+        if (!$request->user() || !$request->user()->hasAdminAccess()) {
             throw new AccessDeniedHttpException();
         }
 
