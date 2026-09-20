f6dd31c1662030720178bb2951c718b402066d88
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-20 02:19:43 +0200

    Nodexa 1.2.1: add register page route

--- resources/scripts/routers/AuthenticationRouter.tsx
+++ resources/scripts/routers/AuthenticationRouter.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch, useRouteMatch } from 'react-router-dom';
 import LoginContainer from '@/components/auth/LoginContainer';
+import RegisterContainer from '@/components/auth/RegisterContainer';
 import ForgotPasswordContainer from '@/components/auth/ForgotPasswordContainer';
 import ResetPasswordContainer from '@/components/auth/ResetPasswordContainer';
 import LoginCheckpointContainer from '@/components/auth/LoginCheckpointContainer';
@@ -16,6 +17,7 @@ export default () => {
         <div className={'pt-8 xl:pt-32'}>
             <Switch location={location}>
                 <Route path={`${path}/login`} component={LoginContainer} exact />
+                <Route path={`${path}/register`} component={RegisterContainer} exact />
                 <Route path={`${path}/login/checkpoint`} component={LoginCheckpointContainer} />
                 <Route path={`${path}/password`} component={ForgotPasswordContainer} exact />
                 <Route path={`${path}/password/reset/:token`} component={ResetPasswordContainer} />
