c4054fabd12f8125e3d1e886cb9fce6e664a80ac
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-20 22:34:22 +0200

    Register Minecraft Mod Manager addon

--- app/Http/Controllers/Admin/AddonController.php
+++ app/Http/Controllers/Admin/AddonController.php
@@ -13,6 +13,7 @@ use Pterodactyl\Models\Egg;
 class AddonController extends Controller
 {
     private const MINECRAFT_PLUGIN_MANAGER = 'minecraft-plugin-manager';
+    private const MINECRAFT_MOD_MANAGER = 'minecraft-mod-manager';
 
     public function __construct(private AlertsMessageBag $alert)
     {
@@ -35,15 +36,31 @@ class AddonController extends Controller
             ]
         );
 
+        $modAddon = Addon::query()->firstOrCreate(
+            ['name' => self::MINECRAFT_MOD_MANAGER],
+            [
+                'game' => 'Minecraft',
+                'category' => 'Mod Manager',
+                'description' => 'Lader Minecraft-serverejere finde og installere mods fra Modrinth filtreret efter Forge, NeoForge, Fabric eller Quilt.',
+                'version' => '1.0',
+                'compatible_versions' => 'Forge, NeoForge, Fabric, Quilt',
+                'download_url' => 'https://modrinth.com',
+                'install_path' => '/mods/',
+                'egg_ids' => null,
+                'enabled' => true,
+            ]
+        );
+
         return view('admin.addons.index', [
             'addon' => $addon,
+            'modAddon' => $modAddon,
             'eggs' => Egg::query()->with('nest')->orderBy('name')->get(),
         ]);
     }
 
     public function show(Addon $addon): View
     {
-        abort_unless($addon->name === self::MINECRAFT_PLUGIN_MANAGER, 404);
+        abort_unless(in_array($addon->name, [self::MINECRAFT_PLUGIN_MANAGER, self::MINECRAFT_MOD_MANAGER], true), 404);
 
         return view('admin.addons.show', [
             'addon' => $addon,
@@ -53,7 +70,7 @@ class AddonController extends Controller
 
     public function update(Request $request, Addon $addon): RedirectResponse
     {
-        abort_unless($addon->name === self::MINECRAFT_PLUGIN_MANAGER, 404);
+        abort_unless(in_array($addon->name, [self::MINECRAFT_PLUGIN_MANAGER, self::MINECRAFT_MOD_MANAGER], true), 404);
 
         $data = $request->validate([
             'egg_ids' => 'nullable|array',
@@ -65,7 +82,7 @@ class AddonController extends Controller
             'enabled' => $request->boolean('enabled'),
         ]);
 
-        $this->alert->success('Minecraft Plugin Manager er opdateret.')->flash();
+        $this->alert->success($addon->name === self::MINECRAFT_MOD_MANAGER ? 'Minecraft Mod Manager er opdateret.' : 'Minecraft Plugin Manager er opdateret.')->flash();
 
         return redirect()->route('admin.addons.show', $addon);
     }
