40b24c05d0edadc80ade2d1afaad8a12e17525da
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-20 21:15:44 +0200

    Polish Nodexa server console UI

--- resources/scripts/components/server/console/ChartBlock.tsx
+++ resources/scripts/components/server/console/ChartBlock.tsx
@@ -10,12 +10,12 @@ interface ChartBlockProps {
 
 export default ({ title, legend, children }: ChartBlockProps) => (
     <div className={classNames(styles.chart_container, 'group')}>
-        <div className={'flex items-center justify-between px-4 py-2'}>
-            <h3 className={'font-header font-medium transition-colors duration-100 group-hover:text-gray-50'}>
+        <div className={'flex items-center justify-between border-b border-gray-700 px-4 py-3'}>
+            <h3 className={'font-header font-semibold text-gray-100 transition-colors duration-100 group-hover:text-gray-50'}>
                 {title}
             </h3>
-            {legend && <p className={'text-sm flex items-center'}>{legend}</p>}
+            {legend && <div className={'text-sm flex items-center'}>{legend}</div>}
         </div>
-        <div className={'z-10 ml-2'}>{children}</div>
+        <div className={'z-10 px-2 pt-3 pb-2'}>{children}</div>
     </div>
 );
