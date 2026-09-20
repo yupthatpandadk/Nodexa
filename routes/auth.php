d5e2cf0cf9f7ed38aec09e7bec2f378b32ed0b3e
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-20 10:18:55 +0200

    Correct malformed RegisterController namespace

--- routes/auth.php
+++ routes/auth.php
@@ -24,7 +24,7 @@ Route::get('/password/reset/{token}', [Auth\LoginController::class, 'index'])->n
 // @see \Pterodactyl\Providers\RouteServiceProvider
 Route::middleware(['throttle:authentication'])->group(function () {
     // Public account registration.
-    Route::post('/register', Auth\\RegisterController::class)->name('auth.post.register')->middleware('recaptcha');
+    Route::post('/register', Auth\RegisterController::class)->name('auth.post.register')->middleware('recaptcha');
 
     // Login endpoints.
     Route::post('/login', [Auth\LoginController::class, 'login'])->middleware('recaptcha');
