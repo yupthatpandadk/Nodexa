cd938dd56ffec22501b9a34d943e3f27db63513b
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-20 21:44:01 +0200

    Add Nodexa Addon Manager

---
+++ app/Models/Addon.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Pterodactyl\Models;
+
+class Addon extends Model
+{
+    protected $table = 'nodexa_addons';
+
+    protected $fillable = [
+        'game', 'category', 'name', 'description', 'version', 'compatible_versions',
+        'download_url', 'install_path', 'egg_ids', 'enabled',
+    ];
+
+    protected $casts = ['enabled' => 'boolean'];
+}
