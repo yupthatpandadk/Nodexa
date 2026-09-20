ac61fc883b74a060479130151c409b3114a9d59b
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-20 21:43:31 +0200

    Add Addon Manager admin routes

--- routes/admin.php
+++ routes/admin.php
@@ -14,6 +14,12 @@ Route::patch('/roles/{role}', [Admin\RoleController::class, 'update'])->name('ad
 Route::delete('/roles/{role}', [Admin\RoleController::class, 'delete'])->name('admin.roles.delete');
 Route::post('/roles/user/{user}', [Admin\RoleController::class, 'assign'])->name('admin.roles.assign');
 
+// Nodexa Addon Manager
+Route::get('/addons', [Admin\\AddonController::class, 'index'])->name('admin.addons');
+Route::post('/addons', [Admin\\AddonController::class, 'store'])->name('admin.addons.store');
+Route::patch('/addons/{addon}', [Admin\\AddonController::class, 'update'])->name('admin.addons.update');
+Route::delete('/addons/{addon}', [Admin\\AddonController::class, 'delete'])->name('admin.addons.delete');
+
 // Nodexa Mail Center
 Route::get('/mail', [Admin\MailCenterController::class, 'index'])->name('admin.mail');
 Route::post('/mail/send', [Admin\MailCenterController::class, 'send'])->name('admin.mail.send');
