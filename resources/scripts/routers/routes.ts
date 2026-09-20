06aa1d75eefdf1cf38eb53b237315e961fb5738c
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-20 22:34:05 +0200

    Add Mod Manager server route

--- resources/scripts/routers/routes.ts
+++ resources/scripts/routers/routes.ts
@@ -14,6 +14,7 @@ import AccountSSHContainer from '@/components/dashboard/ssh/AccountSSHContainer'
 import ActivityLogContainer from '@/components/dashboard/activity/ActivityLogContainer';
 import ServerActivityLogContainer from '@/components/server/ServerActivityLogContainer';
 import PluginManagerContainer from '@/components/server/plugins/PluginManagerContainer';
+import ModManagerContainer from '@/components/server/mods/ModManagerContainer';
 
 // Each of the router files is already code split out appropriately — so
 // all of the items above will only be loaded in when that router is loaded.
@@ -80,6 +81,12 @@ export default {
             permission: 'file.*',
             name: 'Plugins',
             component: PluginManagerContainer,
+        },
+        {
+            path: '/mods',
+            permission: 'file.*',
+            name: 'Mods',
+            component: ModManagerContainer,
         },
         {
             path: '/files',
