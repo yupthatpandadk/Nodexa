7e729bb62dadb16f23244622e9cf61fdb185cfe9
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-22 19:04:36 +0200

    Register advanced client ticket routes

--- routes/storefront.php
+++ routes/storefront.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Pterodactyl\Http\Controllers\StorefrontController;
+use Pterodactyl\Http\Controllers\SupportTicketController;
 
 Route::get('/', [StorefrontController::class, 'index'])->name('store.home');
 Route::get('/hosting', [StorefrontController::class, 'hosting'])->name('store.hosting');
@@ -15,6 +16,12 @@ Route::middleware('auth.session')->group(function () {
     Route::get('/client/servers', [StorefrontController::class, 'clientServers'])->name('store.client.servers');
     Route::get('/client/billing', [StorefrontController::class, 'clientBilling'])->name('store.client.billing');
     Route::get('/client/profile', [StorefrontController::class, 'clientProfile'])->name('store.client.profile');
+    Route::get('/client/tickets', [SupportTicketController::class, 'index'])->name('store.client.tickets');
+    Route::get('/client/tickets/new', [SupportTicketController::class, 'create'])->name('store.client.tickets.create');
+    Route::post('/client/tickets', [SupportTicketController::class, 'store'])->name('store.client.tickets.store');
+    Route::get('/client/tickets/{ticket}', [SupportTicketController::class, 'show'])->name('store.client.tickets.show');
+    Route::post('/client/tickets/{ticket}/reply', [SupportTicketController::class, 'reply'])->name('store.client.tickets.reply');
+    Route::post('/client/tickets/{ticket}/close', [SupportTicketController::class, 'close'])->name('store.client.tickets.close');
     Route::get('/account', [StorefrontController::class, 'dashboard'])->name('store.dashboard');
     Route::post('/store/{product:slug}/order', [StorefrontController::class, 'order'])->name('store.order');
     Route::get('/billing/orders', [StorefrontController::class, 'orders'])->name('store.orders');
