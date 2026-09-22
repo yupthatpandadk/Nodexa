ca14c518e37667941dcff668979859de29d41f2c
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-22 22:49:42 +0200

    Extend ticket messages with internal notes and attachments

--- app/Models/SupportTicketMessage.php
+++ app/Models/SupportTicketMessage.php
@@ -2,8 +2,9 @@
 namespace Pterodactyl\Models;
 class SupportTicketMessage extends Model {
  protected $table='nodexa_support_ticket_messages';
- protected $fillable=['ticket_id','user_id','staff','message'];
- protected $casts=['staff'=>'boolean'];
+ protected $fillable=['ticket_id','user_id','staff','internal','message'];
+ protected $casts=['staff'=>'boolean','internal'=>'boolean'];
  public function ticket(){return $this->belongsTo(SupportTicket::class,'ticket_id');}
  public function user(){return $this->belongsTo(User::class);}
+ public function attachments(){return $this->hasMany(SupportTicketAttachment::class,'message_id');}
 }
