3039dd403bc8833e6a9bb511589dead69ea8a6a7
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-22 17:25:37 +0200

    Fix storefront route registration for Laravel router

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -38,7 +38,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::middleware('web')->group(function () {
-                Route::group(base_path('routes/storefront.php'));
+                Route::group([], base_path('routes/storefront.php'));
 
                 Route::middleware(['auth.session', RequireTwoFactorAuthentication::class])
                     ->group(base_path('routes/base.php'));
