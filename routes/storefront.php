fc656ec635f7047d654878189865eaef0be69da1
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-22 17:18:53 +0200

    Add storefront checkout routes

--- routes/storefront.php
+++ routes/storefront.php
@@ -5,4 +5,8 @@ use Pterodactyl\Http\Controllers\StorefrontController;
 
 Route::get('/store', [StorefrontController::class, 'index'])->name('store.index');
 Route::get('/store/{product:slug}', [StorefrontController::class, 'show'])->name('store.product');
-Route::post('/store/{product:slug}/order', [StorefrontController::class, 'order'])->middleware('auth.session')->name('store.order');
+Route::middleware('auth.session')->group(function () {
+    Route::post('/store/{product:slug}/order', [StorefrontController::class, 'order'])->name('store.order');
+    Route::get('/billing/orders', [StorefrontController::class, 'orders'])->name('store.orders');
+    Route::get('/billing/orders/{order}/checkout', [StorefrontController::class, 'checkout'])->name('store.checkout');
+});
