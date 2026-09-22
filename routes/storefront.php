4dc66678fc700ca781a4ab55c0b18e25bf933863
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-22 18:05:43 +0200

    Make Nodexa storefront the public homepage

--- routes/storefront.php
+++ routes/storefront.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Pterodactyl\Http\Controllers\StorefrontController;
 
+Route::get('/', [StorefrontController::class, 'index'])->name('store.home');
 Route::get('/store', [StorefrontController::class, 'index'])->name('store.index');
 Route::get('/store/{product:slug}', [StorefrontController::class, 'show'])->name('store.product');
 Route::middleware('auth.session')->group(function () {
