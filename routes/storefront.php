4b30b0739cdde259d1497eed0d121248ed4c946e
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-22 18:16:17 +0200

    Add Nodexa multipage storefront routes

--- routes/storefront.php
+++ routes/storefront.php
@@ -4,7 +4,11 @@ use Illuminate\Support\Facades\Route;
 use Pterodactyl\Http\Controllers\StorefrontController;
 
 Route::get('/', [StorefrontController::class, 'index'])->name('store.home');
-Route::get('/store', [StorefrontController::class, 'index'])->name('store.index');
+Route::get('/hosting', [StorefrontController::class, 'hosting'])->name('store.hosting');
+Route::get('/features', [StorefrontController::class, 'features'])->name('store.features');
+Route::get('/about', [StorefrontController::class, 'about'])->name('store.about');
+Route::get('/support', [StorefrontController::class, 'support'])->name('store.support');
+Route::get('/store', [StorefrontController::class, 'hosting'])->name('store.index');
 Route::get('/store/{product:slug}', [StorefrontController::class, 'show'])->name('store.product');
 Route::middleware('auth.session')->group(function () {
     Route::post('/store/{product:slug}/order', [StorefrontController::class, 'order'])->name('store.order');
